0f505872371f4be1610c02bca9317d2797f8955a
Sand038 <sandmdh@gmail.com>
2020-02-14 14:06:04 +0530

    Adding factory patterns examples

---
+++ src/abstractfactory/factory/ComputerFactory.java
@@ -0,0 +1,9 @@
+package abstractfactory.factory;
+
+public class ComputerFactory
+{
+  public static Computer getComputer(ComputerAbstractFactory factory)
+  {
+    return factory.createComputer();
+  }
+}
